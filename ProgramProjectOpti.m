4b3a86f2a50fb2c4cf77ad1c6b4c2770ea28f70c
qlete <quentin.lete@gmail.com>
2015-04-17 14:06:51 +0200

    Specify problem in program

--- ProgramProjectOpti.m
+++ ProgramProjectOpti.m
@@ -2,7 +2,9 @@
 d = importdata('data.mat');
 
 %Création du vecteur objectif (f)
-a = [d.pc d.pc+d.nhb*(d.shs-d.shn) d.ps d.plr d.css];
+%Probléme = Il faut dire que les smartphones livrés en retard doivent quand
+%même être produits
+a = [d.pc d.pc+d.nhb*(d.shs-d.shn) d.ps 1000*d.plr d.css];
 f = a;
 for i = 1:d.nbrSemaine-1
 f = [f a];
@@ -10,13 +12,17 @@ end
 
 %Matrice des contraintes d'égalité
 Aeq = sparse(d.nbrSemaine, d.nbrSemaine*length(a));
+beq = zeros(1,d.nbrSemaine);
 for i = 2:d.nbrSemaine-1
     Aeq(i,(1+length(a)*(i-1)):(length(a)*(i-1)+5)) = [1 1 -1 1 1];
     Aeq(i,length(a)*(i-2)+3) = 1;
+    beq(i) = d.Demande(i);
 end
 Aeq(1,3) = 1;
 Aeq(d.nbrSemaine,(length(a)*d.nbrSemaine-2)) = 1;
-beq = d.Demande; 
+beq(1) = 0;
+beq(d.nbrSemaine) = 0;
+full(Aeq)
 
 %Matrice des contraintes d'inégalité
 A = sparse(2*d.nbrSemaine, d.nbrSemaine*length(a));
