875af4d5d5adfca4b495d29c55757fc620ea23e3
qlete <quentin.lete@gmail.com>
2015-05-09 10:30:25 +0200

    Solve last week problem

--- ProgramProjectOpti.m
+++ ProgramProjectOpti.m
@@ -4,6 +4,10 @@ emplDepart = d.nb_ouvriers;
 %Remise des données sous la forme stock-initial;demande;stockfinal pour
 %faciliter la programmation. On ajoute donc deux semaines qui correspondent
 %à la semaine 0 qui ne contient que le stock initial, et la semaine T+1 qui contient le stock final.
+%Remarque : Cette deernière ligne n'a pas d'utilité mais sert seulement à
+%améliorer la lisibilité des réponses. Elle reprend exactement le stock
+%précédent dans son stock et les productions sont nulles. C'est une semaine
+%fictive, tout comme la prmière.
 d.demande = [d.stock_initial d.demande d.stock_initial];
 d.T = d.T+2;
 
@@ -18,7 +22,7 @@ end
 Aeq = sparse(d.T, d.T*length(a));
 beq = zeros(1,d.T);
 beq(1) = d.demande(1);
-beq(d.T) = d.demande(d.T);
+beq(d.T) = 0; %Pour que le stock revienne à stock_initial
 for i = 2:d.T-1
     Aeq(i,(1+length(a)*(i-1)):(length(a)*(i-1)+5)) = [1 1 -1 1 1];
     Aeq(i,length(a)*(i-2)+3) = 1;
@@ -26,7 +30,9 @@ for i = 2:d.T-1
     beq(i) = d.demande(i);
 end
 Aeq(1,3) = 1;
-Aeq(d.T,(length(a)*d.T-2)) = 1;
+%Indique que le stock de la dernière ligne est simplement celui d'avant
+Aeq(d.T,(length(a)*d.T-2)) = 1; 
+Aeq(d.T,(length(a)*d.T-7)) = -1;
 %full(Aeq)
 
 %Matrice des contraintes d'inégalité
@@ -44,7 +50,7 @@ for i = d.T+1:2*d.T
 end
 %Ajoute l'inegalite x(i-1,r) <= x(i,n) + x(i,sup) + x(i,sst)
 %qui dit que l'on doit quand meme produire les sp que l'on rend en retard
-for i = 2*d.T+1:3*d.T
+for i = 2*d.T+1:3*d.T-1
     if i ~= 2*d.T+1 %La première semaine, pas encore de sp en retard
     A(i, (length(a)*(i-2*d.T-1)-1)) = 1;
     end
@@ -55,15 +61,23 @@ end
 
 
 %intlinprog(f,1:length(f),A,b,Aeq,beq,zeros(length(f)),[])
-maxsst = zeros(size(f));
+ub = zeros(size(f)); %% Upper bound sur les variables
+lb = zeros(size(f)); %% Lower bound sur les variables
 for i = 1:length(f)
     if mod(i,5) == 0
-        maxsst(i) = d.nb_max_sous_traitant;
+        ub(i) = d.nb_max_sous_traitant;
     else
-        maxsst(i) = inf;
+        ub(i) = inf;
+    end
+    if i == ((length(f))/5-2)*5+3 %Oblige le stock à revenir au stock initial à la fin de la planification
+        ub(i) = d.stock_initial;
+        lb(i) = d.stock_initial;
+    end
+    if i == ((length(f))/5-2)*5+4 %On peut pas rendre en retard la dernière semaine !
+        ub(i) = 0;
     end
 end
-options = optimoptions('linprog', 'Algorithm', 'simplex');
+options = optimoptions('linprog', 'Algorithm', 'simplex');%Pour qu'il nous donne bien une sol entière s'il y en a une
 
-reshape(linprog(f,A,b,Aeq,beq,zeros(size(f)),maxsst,zeros(size(f)),options),5,17)'
+reshape(linprog(f,A,b,Aeq,beq,lb,ub,zeros(size(f)),options),5,17)'
 %reshape(linprog(f,A,b,Aeq,beq,zeros(size(f)),[]),5,17)'
