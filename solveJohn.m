8ce487dd544e75d19147f8a221a61c58b4c19e80
John de Wasseige <johndewasseige@me.com>
2015-05-12 19:22:17 +0200

    in- and output args in solvejohn

--- solveJohn.m
+++ solveJohn.m
@@ -1,9 +1,24 @@
-function solveJohn
+function [X,fval] = solveJohn(donnees)
 %SOLVEJOHN - Implementation et solution du modele lineaire continu de 
 %            la ligne d'assemblage simple (personnel constant).
 
+% input    - donnees : si aucun vecteur de donnees n'est utilise en
+%                      argument, alors on utilise le fichier 'donnees.mat'
+%
+% output   - X     : matrice de taille Tx5 ou l'element x_(i,j)
+%                      correspond au nombre de smartphones de type j
+%                      produits pendant la semaine i
+%                      j correspond a un des types suivants
+%                      [x_normal x_suplementaire x_stock x_retard x_sst]
+%          - fval  : valeur du cout optimal
+
 %% load data, fonction objectif et constantes
-d = importdata('donnees.mat');
+if nargin==0
+    d = importdata('donnees.mat');
+else
+    d = donnees
+end
+
 T = d.T; % nombre de semaines
 
 % fonction objectif 
