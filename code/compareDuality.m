c32cb58c70e7287d4774f9c30ebacbe77e0293de
John de Wasseige <johndewasseige@me.com>
2015-05-14 01:54:37 +0200

    easier comparaison btw primal and dual

--- code/compareDuality.m
+++ code/compareDuality.m
@@ -2,19 +2,31 @@ function compareDuality
 
 d = importdata('donnees.mat');
 
-compareDualitySpeed
+%close all;
+
+%compareDualitySpeed;
 compareDualityValues
 
 
 %% compare couts
     function compareDualityValues
-    n = 11;
+    n = 10;
     epsilon = linspace(0,1,n);
-    cout_primal = multiplePrimal(n,d);
-    cout_dual = multipleDual(n,d);
+    
+    cout_primal = multiplePrimal(n,epsilon);
+    cout_dual = multipleDual(n,epsilon);
+    
     for i=1:n
         fprintf('%d \t %d \n', cout_primal(i), cout_dual(i));
     end
+    
+    figure
+    plot(epsilon,cout_primal,'.-r',epsilon,cout_dual,'.-b');
+    xlabel('epsilon');
+    ylabel('cout');
+    legend('primal','dual');
+    title('Effets des perturbations sur le cout'); hold off;
+
     end
 
 %% compare efficiency
@@ -33,14 +45,13 @@ compareDualityValues
     xlabel('taille de epsilon');
     ylabel('CPU time');
     legend('primal','dual');
-    title('Computational efficiency');
+    title('Computational efficiency'); hold off;
 
     end
 
 %% multiplePrimal
-    function couts = multiplePrimal(n)
+    function couts = multiplePrimal(n,epsilon)
     couts = zeros(n,1);
-    epsilon = linspace(0,1,n);
     dPrimal = d;
 
     for i=1:n
@@ -52,15 +63,14 @@ compareDualityValues
     end
 
 %% multipleDual
-    function couts = multipleDual(n)
+    function couts = multipleDual(n,epsilon)
     couts = zeros(n,1);
-    epsilon = linspace(0,1,n);
 
-    [valOptimum,x_dual,f_dual] = solveDualJohn(d,0);
+    [~,x_dual,f_dual] = solveDualJohn(d,0);
     newObj = f_dual;
     for i=1:n
         newObj(1:d.T) = f_dual(1:d.T) + epsilon(i)*d.delta_demande' ;
-        couts(i) = newObj'*x_dual;
+        couts(i) = newObj'*x_dual + 35*d.nb_ouvriers*d.cout_horaire;
         fprintf('cout dual = %d \n',couts(i));
     end
     
