5e881c702e5ac51c23175a4d9a77fd15cede8eb1
John de Wasseige <johndewasseige@me.com>
2015-06-14 15:15:58 +0200

    improve plotGraphes

--- code/plotGraphes.m
+++ code/plotGraphes.m
@@ -2,40 +2,33 @@ function plotGraphes(data,var)
 
 data= importdata(data);
 
-if var=='3'
-    [~,X] = question3(data);
-    T = repmat((0:1:data.T)',1,5);
-
-elseif var=='5'
+if var=='5'
     question5;
+else
+
+    if var=='3'
+        [~,X] = question3(data);
+    elseif var=='8'
+        [~,X] = question8(data);
+    else
+        [~,X] = question9(data);
+    end
     
-elseif var=='8'
-    [~,X] = question8(data);
-    T = repmat((0:1:data.T)',1,8);
+    [~,n] = size(X);
+    T = repmat((0:1:data.T)',1,n);
 
-else
-    [~,X] = question9(data);
-    T = repmat((0:1:data.T)',1,8);
 
-end
+    figure;
+    plot(T,X);
+    xlabel('semaine');
 
-T = T
-X = X 
-
-figure;
-plot(T,X);
-xlabel('semaine');
-
-if var=='3'
-    legend('x_n','x_sup','x_st','x_ret','x_sst');
-    title('Solution a personnel constant'); hold off;
-elseif var=='8'
-    legend('x_n','x_sup','x_st','x_ret','x_sst','n_ouv','n_emb','n_lic');
-    title('Solution a personnel variable'); hold off;
-
-else
-    legend('x_n','x_sup','x_st','x_ret','x_sst','n_ouv','n_emb','n_lic');
-    title('Solution a personnel variable'); hold off;
+    if var=='3'
+        legend('x_n','x_sup','x_st','x_ret','x_sst');
+        title('Solution a personnel constant'); hold off;
+    else
+        legend('x_n','x_sup','x_st','x_ret','x_sst','n_ouv','n_emb','n_lic');
+        title('Solution a personnel variable'); hold off;
+    end
 
 end
 
